0b9aef51a47bda4944e26e697c82214ab9892467
kmichaelk <130953568+kmichaelk@users.noreply.github.com>
2024-03-16 15:46:31 +0300

    Stable texture-based renderer

--- src/renderer/texture/index.ts
+++ src/renderer/texture/index.ts
@@ -17,10 +17,11 @@ export const TextureRenderer: RendererInitializer = (gl: WebGLRenderingContext):
       { type: gl.FRAGMENT_SHADER, source: shaderSourceFragment }
     ],
     (gl, program) => ({
-      vertexPosition: gl.getAttribLocation(program, 'a_position'),
-      vertexTexcoord: gl.getAttribLocation(program, 'a_texcoord'),
+      position: gl.getAttribLocation(program, 'a_position'),
+      texcoord: gl.getAttribLocation(program, 'a_texcoord'),
     }),
     (gl, program) => ({
+      resolution: gl.getUniformLocation(program, 'u_resolution')!,
       texture: gl.getUniformLocation(program, 'u_texture')!
     })
   )
@@ -30,6 +31,7 @@ export const TextureRenderer: RendererInitializer = (gl: WebGLRenderingContext):
     'texcoord',
   ])
 
+  let textureBuffer: Uint8Array
   const texture = gl.createTexture()!
 
   return Base2DRenderer({
@@ -43,8 +45,8 @@ export const TextureRenderer: RendererInitializer = (gl: WebGLRenderingContext):
         tomogram.size.x, 0,
         tomogram.size.x, tomogram.size.y,
       ]), gl.STATIC_DRAW)
-      gl.vertexAttribPointer(attribs.vertexPosition, 2, gl.FLOAT, false, 0, 0)
-      gl.enableVertexAttribArray(attribs.vertexPosition)
+      gl.vertexAttribPointer(attribs.position, 2, gl.FLOAT, false, 0, 0)
+      gl.enableVertexAttribArray(attribs.position)
 
       gl.bindBuffer(gl.ARRAY_BUFFER, buffers.texcoord)
       gl.bufferData(gl.ARRAY_BUFFER, new Float32Array([
@@ -55,12 +57,14 @@ export const TextureRenderer: RendererInitializer = (gl: WebGLRenderingContext):
         1.0, 0.0,
         1.0, 1.0,
       ]), gl.STATIC_DRAW)
-      gl.vertexAttribPointer(attribs.vertexTexcoord, 2, gl.FLOAT, false, 0, 0)
-      gl.enableVertexAttribArray(attribs.vertexTexcoord)
+      gl.vertexAttribPointer(attribs.texcoord, 2, gl.FLOAT, false, 0, 0)
+      gl.enableVertexAttribArray(attribs.texcoord)
 
       gl.useProgram(program)
 
-      gl.uniform1i(uniforms.texture, 0);
+      gl.uniform2f(uniforms.resolution, gl.canvas.width, gl.canvas.height)
+      gl.uniform1i(uniforms.texture, 0)
+
       gl.drawArrays(gl.TRIANGLES, 0, 6);
     },
     changeTriggersCacheUpdate: [
@@ -78,27 +82,32 @@ export const TextureRenderer: RendererInitializer = (gl: WebGLRenderingContext):
         transferFunctionWidth = config.transferFunctionWidth
       }
 
-      gl.bindTexture(gl.TEXTURE_2D, texture)
-      //
       const size = tomogram.size.x * tomogram.size.y
-      const texBuffer = new Uint8Array(3 * size)
-      for (let i = 0, offset = config.layer * size; i < size; i += 4, offset++) {
-        texBuffer[i + 0] = cache[offset]
-        texBuffer[i + 1] = cache[offset]
-        texBuffer[i + 2] = cache[offset]
+      const pixels = size * 3
+      const bufSize = 3 * pixels
+      if (textureBuffer?.length != bufSize) {
+        textureBuffer = new Uint8Array(bufSize)
       }
-      //
+      for (let i = 0, offset = config.layer * size; i < pixels; i += 3, offset++) {
+        textureBuffer[i + 0] = cache[offset]
+        textureBuffer[i + 1] = cache[offset]
+        textureBuffer[i + 2] = cache[offset]
+      }
+      console.log(textureBuffer.length)
+      
+      gl.bindTexture(gl.TEXTURE_2D, texture)
       gl.texImage2D(
         gl.TEXTURE_2D,
         0,
         gl.RGB,
-        1,
-        1,
+        tomogram.size.x,
+        tomogram.size.y,
         0,
         gl.RGB,
         gl.UNSIGNED_BYTE,
-        texBuffer,
+        textureBuffer,
       );
+      
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.LINEAR)
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.LINEAR)
       gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE)
--- src/renderer/texture/shaders/vertex.glsl
+++ src/renderer/texture/shaders/vertex.glsl
@@ -1,9 +1,11 @@
 attribute vec2 a_position;
 attribute vec2 a_texcoord;
 
+uniform vec2 u_resolution;
+
 varying vec2 v_texcoord;
 
 void main(void) {
-    gl_Position = vec4(a_position, 0.0, 1.0);
+    gl_Position = vec4((((a_position / u_resolution) * 2.0) - 1.0), 0.0, 1.0);
     v_texcoord = a_texcoord;
 }
