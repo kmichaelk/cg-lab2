697722a2965e2440fec3818ca4c6285f9a0739d3
kmichaelk <130953568+kmichaelk@users.noreply.github.com>
2024-03-16 13:32:40 +0300

    Use medium precision for quad colors

--- src/renderer/quads/shaders/fragment.glsl
+++ src/renderer/quads/shaders/fragment.glsl
@@ -1,4 +1,4 @@
-varying highp float v_color;
+varying mediump float v_color;
 
 void main(void) {
     gl_FragColor = vec4(v_color, v_color, v_color, 1);
--- src/renderer/quads/shaders/vertex.glsl
+++ src/renderer/quads/shaders/vertex.glsl
@@ -1,7 +1,7 @@
 attribute vec2 a_position;
 attribute float a_color;
 
-varying highp float v_color;
+varying mediump float v_color;
 
 void main(void) {
     gl_Position = vec4(a_position, 0.0, 1.0);
